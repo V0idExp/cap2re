e9d5454c6d321f9e485284c0b20e99ec046f3e89
Ivan Nikolaev <voidexp@gmail.com>
2011-11-17 18:17:11 +0100

    Added an initial support for std::vector for Python

--- src/CommonWrapper.h
+++ src/CommonWrapper.h
@@ -3,11 +3,44 @@
 
 #include <vector>
 #include <boost/python.hpp>
+#include "Error.h"
 
-template<typename ValueType>
-struct PyVector
+#include <iostream>
+using namespace std;
+
+template<class ValueType>
+struct Vector2List
 {
+    static ValueType
+    getitem(const std::vector<ValueType> &self, int index)
+    {
+        if(index < 0)
+            index += self.size();
 
+        cout << "Requesting element " << index << endl;
+        if(index >= 0 && index < self.size())
+            return self[index];
+
+        throw RuntimeError("index out of range");
+
+    }
+
+    static int
+    len(const std::vector<ValueType> &self)
+    {
+        return self.size();
+    }
+
+    iterator(const std::vector<ValueType> &self)
+    {
+
+    }
+
+    static void
+    indexError()
+    {
+        PyErr_SetString(PyExc_IndexError, "Index out of range");
+    }
 };
 
 #endif // COMMONWRAPPER_H
